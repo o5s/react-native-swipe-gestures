1f36d108c49dea6dc8ced641fc09fc5b3a134b2e
IAN OOI <ye221090@gmail.com>
2021-02-22 23:37:29 +0800

    Update swipe detection disable config

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ export const swipeDirections = {
 const swipeConfig = {
   velocityThreshold: 0.3,
   directionalOffsetThreshold: 80,
-  gestureIsClickThreshold: 5
+  gestureIsClickThreshold: 5,
+  detectSwipeUp: true,
+  detectSwipeDown: true,
+  detectSwipeLeft: true,
+  detectSwipeRight: true
 };
 
 function isValidSwipe(
@@ -42,17 +46,35 @@ class GestureRecognizer extends Component {
       onPanResponderTerminate: responderEnd
     });
   }
-  
+
   componentDidUpdate(prevProps) {
     if (this.props.config !== prevProps.config) {
       this.swipeConfig = Object.assign(swipeConfig, this.props.config);
     }
   }
-  
+
   _handleShouldSetPanResponder(evt, gestureState) {
     return (
       evt.nativeEvent.touches.length === 1 &&
-      !this._gestureIsClick(gestureState)
+      !this._gestureIsClick(gestureState) &&
+      this._validateSwipe(gestureState)
+    );
+  }
+
+  _validateSwipe(gestureState) {
+    const {
+      detectSwipeUp,
+      detectSwipeDown,
+      detectSwipeLeft,
+      detectSwipeRight
+    } = this.swipeConfig;
+    const swipeDirection = this._getSwipeDirection(gestureState);
+    const { SWIPE_LEFT, SWIPE_RIGHT, SWIPE_UP, SWIPE_DOWN } = swipeDirections;
+    return (
+      (detectSwipeUp && swipeDirection === SWIPE_UP) ||
+      (detectSwipeDown && swipeDirection === SWIPE_DOWN) ||
+      (detectSwipeLeft && swipeDirection === SWIPE_LEFT) ||
+      (detectSwipeRight && swipeDirection === SWIPE_RIGHT)
     );
   }
 
